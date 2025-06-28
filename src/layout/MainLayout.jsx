0ca183374f169c58f49a441ef7f3736fa9cdce7d
Jubayer Shikder <validjubayer@gmail.com>
2025-06-28 10:00:35 +0600

    Add logo

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,9 +1,11 @@
+import { Outlet } from "react-router";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
 
 const MainLayout = () => {
   return (
     <div className="min-h-screen flex flex-col dark:bg-black">
       <Navbar />
-      <hr className="text-gray-200" />
       <main className="flex-grow flex flex-col justify-center mb-15">
         <Outlet />
       </main>
