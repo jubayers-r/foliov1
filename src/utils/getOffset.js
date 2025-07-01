173580daf94f6b6b2befbb9de9d2173985615fda
Jubayer Shikder <validjubayer@gmail.com>
2025-07-02 00:31:34 +0600

    minor changes

--- src/utils/getOffset.js
+++ src/utils/getOffset.js
@@ -1,14 +1,15 @@
-  export const getOffset = (section) => {
-    // Example: Different offset for "contact", or based on window width
-    if (window.innerWidth < 640 && section === "skills") return -250;
-    if (
-      section === "projects" ||
-      section === "contact" ||
-      (window.innerWidth < 640 && section === "me") ||
-      (window.innerWidth < 640 && section === "about") ||
-      (window.innerWidth < 640 && section === "contact")
-    )
-      return -100;
-    // if (window.innerWidth < 640) return -150; // Small screens
-    return -230; // Default
-  };
+export const getOffset = (section) => {
+  // Example: Different offset for "contact", or based on window width
+  if (window.innerWidth < 640 && section === "skills") return -250;
+  if (
+    section === "projects" ||
+    section === "contacts" ||
+    (window.innerWidth < 640 && section === "me") ||
+    (window.innerWidth < 640 && section === "about")
+  )
+    return -100;
+  if (section === "contact") return -100;
+  if (window.innerWidth < 640 && section === "contact") return -150;
+  // if (window.innerWidth < 640) return -150; // Small screens
+  return -230; // Default
+};
