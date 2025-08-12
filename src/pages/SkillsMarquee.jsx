899fd1aad9c4e14b08886817208097b722fde66b
Jubayer Shikder <validjubayer@gmail.com>
2025-08-12 09:09:58 +0600

    chore: add more tech logo

--- src/pages/SkillsMarquee.jsx
+++ src/pages/SkillsMarquee.jsx
@@ -1,5 +1,5 @@
-
 import FastMarquee from "../components/FastMarquee";
+import { Element } from "react-scroll";
 
 const techLogoImports = import.meta.glob(
   "/src/assets/technologies/*.{jpg,jpeg,png,svg,webp}",
@@ -20,38 +20,39 @@ const techLogos = Object.entries(techLogoImports)
 
 // js(), react(), nodejs(), express(), tailwind(), framer motion(), mongodb(), firebase(), github(), vscode(), daisyui(), vite(), react router(), jwt(), vercel(), netlify(), pritter(), eslint(), bash()
 
-// next to add: pnpm/yarn/bun, TypeScript, nextjs, postman, linux, redux, git, OAuth/Auth0, python, Stripe , sslcommerz, mysql, orms, docker, vim, Prisma, socketio, webrtc, jest/vitest/Cypress, graphql, gsap, React Three Fiber, figma, wordpress, elementor pro, framer, shopify
+// next to add: yarn/bun,  python, socketio, webrtc, jest/vitest/Cypress, graphql, gsap, React Three Fiber, figma, wordpress, elementor pro, framer, shopify
 
+// , , OAuth/Auth0
 const SkillsMarquee = () => {
   return (
-    <div id="skills" data-aos="fade-down">
-    <>
-      <section className="h-fit my-50 max-w-10/11 md:max-w-9/11 overflow-hidden mx-auto " >
-        <h1 className=" text-center text-2xl md:text-3xl xl:text-4xl font-semibold">
-          Recent Technologies I’ve Used
-        </h1>
-        <div className="mt-6 [mask-image:linear-gradient(to_right,transparent,black_20%,black_80%,transparent)]">
-          <FastMarquee gradient={false} speed={50}>
-            <div className="flex gap-12 w-max pointer-events-none">
-              {[...techLogos, ...techLogos].map((src, idx) => (
-                <div
-                  key={idx}
-                  className="w-[70px] h-[70px] relative hover:scale-105 transition-transform duration-150 ease-in-out"
-                >
-                  <img
-                    src={src}
-                    alt="tech-icon"
-                    className="absolute w-full h-full object-contain inset-0"
-                    loading="lazy"
-                  />
-                </div>
-              ))}
-            </div>
-          </FastMarquee>
-        </div>
-      </section>
-    </>
-    </div>
+    <Element id="skills" data-aos="fade-down">
+      <>
+        <section className="h-fit my-50 max-w-10/11 md:max-w-9/11 overflow-hidden mx-auto ">
+          <h1 className=" text-center text-2xl md:text-3xl xl:text-4xl font-semibold">
+            Tools & Technologies I Use
+          </h1>
+          <div className="mt-6 [mask-image:linear-gradient(to_right,transparent,black_20%,black_80%,transparent)]">
+            <FastMarquee gradient={false} speed={50}>
+              <div className="flex w-max pointer-events-none">
+                {[...techLogos, ...techLogos].map((src, idx) => (
+                  <div
+                    key={idx}
+                    className="w-[50px] lg:w-[70px] h-[50px] lg:h-[70px] relative hover:scale-105 transition-transform duration-150 ease-in-out ml-7 lg:ml-15"
+                  >
+                    <img
+                      src={src}
+                      alt="tech-icon"
+                      className="absolute w-full h-full object-contain inset-0"
+                      loading="lazy"
+                    />
+                  </div>
+                ))}
+              </div>
+            </FastMarquee>
+          </div>
+        </section>
+      </>
+    </Element>
   );
 };
 
